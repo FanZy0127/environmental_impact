ff8a49b743b3ce08295d1543ad86cad9461ed46a
baptiste-3wa <baptiste@3wa.io>
2023-05-24 16:45:44 +0200

    Adding of CO2 equivalence variables which need to be compleated.

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,61 @@ class Settings:
     DATA_EU_AIR_F1_2 = os.getenv("DATA_EU_AIR_F1_2")
     DATA_EU_AIR_F1_3 = os.getenv("DATA_EU_AIR_F1_3")
     DATA_EU_AIR_F1_4 = os.getenv("DATA_EU_AIR_F1_4")
-    
+
+    CO2_CO2_Factor_Equivalence_20_Years = 1
+
+    # CO2 Factor Equivalence on a 20 years base
+    NMVOC_CO2_Factor_Equivalence_20_Years = 5.83  # consensus non défini, donc à vérifier
+    NOX_CO2_Factor_Equivalence_20_Years = 0
+    SOX_CO2_Factor_Equivalence_20_Years = 0
+    CO_CO2_Factor_Equivalence_20_Years = 0
+    CH4_CO2_Factor_Equivalence_20_Years = 0
+    Hg_CO2_Factor_Equivalence_20_Years = 0
+    PM10_CO2_Factor_Equivalence_20_Years = 0
+    N2O_CO2_Factor_Equivalence_20_Years = 0
+    HCFCs_CO2_Factor_Equivalence_20_Years = 0
+    Cr_CO2_Factor_Equivalence_20_Years = 0
+    Zn_CO2_Factor_Equivalence_20_Years = 0
+    Cu_CO2_Factor_Equivalence_20_Years = 0
+    Pb_CO2_Factor_Equivalence_20_Years = 0
+    NH3_CO2_Factor_Equivalence_20_Years = 0
+    Benzene_CO2_Factor_Equivalence_20_Years = 0
+    DCM_CO2_Factor_Equivalence_20_Years = 0
+    Cd_CO2_Factor_Equivalence_20_Years = 0
+    Ni_CO2_Factor_Equivalence_20_Years = 0
+    Teq_CO2_Factor_Equivalence_20_Years = 0
+    HFCS_CO2_Factor_Equivalence_20_Years = 0
+    HCl_CO2_Factor_Equivalence_20_Years = 0
+    As_CO2_Factor_Equivalence_20_Years = 0
+    HF_CO2_Factor_Equivalence_20_Years = 0
+    CO2_Excluding_Biomass_CO2_Factor_Equivalence_20_Years = 0
+    CFCs_CO2_Factor_Equivalence_20_Years = 0
+
+    # CO2 Factor Equivalence on a 100 years base
+    NMVOC_CO2_Factor_Equivalence_100_Years = 2.36  # consensus non défini, donc à vérifier
+    NOX_CO2_Factor_Equivalence_100_Years = 0
+    SOX_CO2_Factor_Equivalence_100_Years = 0
+    CO_CO2_Factor_Equivalence_100_Years = 0
+    CH4_CO2_Factor_Equivalence_100_Years = 0
+    Hg_CO2_Factor_Equivalence_100_Years = 0
+    PM10_CO2_Factor_Equivalence_100_Years = 0
+    N2O_CO2_Factor_Equivalence_100_Years = 0
+    HCFCs_CO2_Factor_Equivalence_100_Years = 0
+    Cr_CO2_Factor_Equivalence_100_Years = 0
+    Zn_CO2_Factor_Equivalence_100_Years = 0
+    Cu_CO2_Factor_Equivalence_100_Years = 0
+    Pb_CO2_Factor_Equivalence_100_Years = 0
+    NH3_CO2_Factor_Equivalence_100_Years = 0
+    Benzene_CO2_Factor_Equivalence_100_Years = 0
+    DCM_CO2_Factor_Equivalence_100_Years = 0
+    Cd_CO2_Factor_Equivalence_100_Years = 0
+    Ni_CO2_Factor_Equivalence_100_Years = 0
+    Teq_CO2_Factor_Equivalence_100_Years = 0
+    HFCS_CO2_Factor_Equivalence_100_Years = 0
+    HCl_CO2_Factor_Equivalence_100_Years = 0
+    As_CO2_Factor_Equivalence_100_Years = 0
+    HF_CO2_Factor_Equivalence_100_Years = 0
+    CO2_Excluding_Biomass_CO2_Factor_Equivalence_100_Years = 0
+    CFCs_CO2_Factor_Equivalence_100_Years = 0
 
 settings = Settings()
